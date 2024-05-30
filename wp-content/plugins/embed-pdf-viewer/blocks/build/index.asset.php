b63c21857fac8ecffbc4123adfa1a0a662dc0d8c
Rozh Ali <rozhbasim7@gmail.com>
2024-05-30 21:57:59 +0300

    removed

--- wp-content/plugins/embed-pdf-viewer/blocks/build/index.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('wp-element', 'wp-polyfill'), 'version' => '5c7c231933cb979dc009e4397c48052a');
--- wp-content/plugins/pdf-embed/build/view.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array(), 'version' => 'c6c45ce8d90c7df85524');
